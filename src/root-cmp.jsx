51f9ca4ecc47089ab690d10930439c1722491f54
inbal <inbalmoshe49@gmail.com>
2022-09-18 09:57:12 +0300

    add filter

--- src/root-cmp.jsx
+++ src/root-cmp.jsx
@@ -2,18 +2,14 @@ import React from "react";
 import { Routes, Route } from 'react-router'
 // import { AppHeader } from "./cmps/app-header";
 import routes from './routes'
-import {AppHeader} from './cmps/app-header'
-import {AppFooter} from './cmps/app-footer'
+import { AppHeader } from './cmps/app-header'
+import { AppFooter } from './cmps/app-footer'
 
 
 export class RootCmp extends React.Component {
   render() {
     return (
-<<<<<<< HEAD
       <div className="main-container">
-=======
-      <div  className="main-container" >
->>>>>>> 3e69fae230a774594ae6a1f9447348f25490b481
         <AppHeader />
         <main>
           <Routes>
@@ -21,9 +17,9 @@ export class RootCmp extends React.Component {
             {/* <Route path="user/:id" element={<UserDetails />} /> */}
           </Routes>
         </main>
-        <AppFooter />   
+        <AppFooter />
       </div>
-    );
+    )
   }
 
 }
